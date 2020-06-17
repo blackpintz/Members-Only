b67602f228b6df314000e0559aebcdafec680993
Ubuntu <ubuntu@ip-172-31-24-11.us-east-2.compute.internal>
2020-06-17 10:10:14 +0000

    filled out post controller actions-new,create and index

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,10 +1,28 @@
 class PostsController < ApplicationController
-	before_action :authenticate_user!, except: [:index]
+	before_action :authenticate_author!, except: [:index]
 
 	def index
+		@posts = Post.all
 	end
 
 	def new
-		@post=Post.new
+		@post=current_author.post.build
+	end
+	
+	def create
+	  @post=current_author.post.build(post_params)
+	    respond_to do |format|
+		      if @post.save
+		        format.html { redirect_to root_path, notice: 'Post was successfully created.' }
+		      else
+		        format.html { render :new }
+		      end
+    	end	
+	end
+	
+	private
+	
+	def post_params
+		params.require(:post).permit(:title,:body)
 	end
 end
